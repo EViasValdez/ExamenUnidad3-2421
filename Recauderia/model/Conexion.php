a235d4696ecfed666fc843fed14f0ba7b1772fbb
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2024-12-04 11:53:31 -0600

    Update Conexion.php

--- Recauderia/model/Conexion.php
+++ Recauderia/model/Conexion.php
@@ -1,13 +1,11 @@
 <?php
-    $server="162.241.62.132";
-    $usuario="yourlach_root";
-    $password="h-bG(jU{}AA,";
-    $database="yourlach_recauderia2";
-    $conectar=mysqli_connect($server , $usuario , $password , $database);
+    $server = "162.241.62.132";
+    $usuario = "yourlach_root";
+    $password = "h-bG(jU{}AA,";
+    $database = "yourlach_recauderia2";
+    $conectar = mysqli_connect($server , $usuario , $password , $database);
         
-    if(!$conectar){
+    if (!$conectar) {
         die("Error de coneccion: ".mysqli_connect_error());
-    
     }
-
 ?>
