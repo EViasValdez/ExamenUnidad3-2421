15692c918185deffd9137e0eab42f1db02d5dbd0
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2024-12-04 11:53:03 -0600

    Update Update.php

--- Recauderia/controller/Update.php
+++ Recauderia/controller/Update.php
@@ -3,10 +3,10 @@
 include("../model/conexion2.php");
 $con=conectar();
 
-$id=$_POST['id'];
-$nombres=$_POST['nombre'];
-$tipo=$_POST['tipo'];
-$cantidad=$_POST['cantidad'];
+$id = $_POST['id'];
+$nombres = $_POST['nombre'];
+$tipo = $_POST['tipo'];
+$cantidad = $_POST['cantidad'];
 
 $sql="UPDATE almacen SET  nombre='$nombres', tipo='$tipo',cantidad='$cantidad' WHERE id='$id'";
 $query=mysqli_query($con,$sql);
