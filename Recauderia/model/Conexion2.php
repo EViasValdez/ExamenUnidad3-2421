09255aebb01f08d3235cd48dd4171b9413dbeae6
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2024-12-04 11:53:54 -0600

    Update Conexion2.php

--- Recauderia/model/Conexion2.php
+++ Recauderia/model/Conexion2.php
@@ -1,12 +1,12 @@
 <?php
-function conectar(){
-    $host="162.241.62.132";
-    $user="yourlach_root";
-    $pass="h-bG(jU{}AA,";
+function conectar () {
+    $host = "162.241.62.132";
+    $user = "yourlach_root";
+    $pass = "h-bG(jU{}AA,";
 
     $bd="yourlach_recauderia2";
 
-    $con=mysqli_connect($host,$user,$pass);
+    $con = mysqli_connect($host,$user,$pass);
 
     mysqli_select_db($con,$bd);
 
