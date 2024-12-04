a60dc9a0858b2501c8a0124f5aa9afb3aa33a847
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2024-12-04 11:50:55 -0600

    Update Controlador.php

--- Recauderia/controller/Controlador.php
+++ Recauderia/controller/Controlador.php
@@ -12,13 +12,10 @@
 include("../model/conexion2.php");
 $con=conectar();
 
-
-
 $id=$_POST['id'];
 $nombre=$_POST['nombre'];
 $tipo=$_POST['tipo'];
 $cantidad=$_POST['cantidad'];
-
 
 $sql="INSERT INTO `almacen`(`id`, `nombre`, `tipo`, `cantidad`) VALUES('$id','$nombre','$tipo','$cantidad')";
 $query= mysqli_query($con,$sql);
@@ -29,7 +26,6 @@ if($query){
 }else {
 }
 ?>
-
 
 </body>
 </html>
