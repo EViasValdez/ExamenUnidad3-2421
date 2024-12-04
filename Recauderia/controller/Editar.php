a2dd2412c0d93dffd1f70e0d594a0a9cde392a8d
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2024-12-04 11:52:22 -0600

    Update Editar.php

--- Recauderia/controller/Editar.php
+++ Recauderia/controller/Editar.php
@@ -1,9 +1,8 @@
 <?php 
     include("../model/conexion2.php");
-    $con=conectar();
+    $con = conectar();
 
 $id=$_GET['id'];
-
 $sql="SELECT * FROM almacen WHERE id='$id'";
 $query=mysqli_query($con,$sql);
 
@@ -19,7 +18,6 @@ $row=mysqli_fetch_array($query);
         <link href="css/style.css" rel="stylesheet">
         <title>Actualizar</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-        
     </head>
     <body>
                 <div class="container mt-5">
