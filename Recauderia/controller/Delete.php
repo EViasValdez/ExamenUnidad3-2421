9c2ab1514e5c1dfd7ff0d9392c655b122e00deee
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2025-04-11 18:05:00 -0600

    Actualizar Delete.php

--- Recauderia/controller/Delete.php
+++ Recauderia/controller/Delete.php
@@ -2,10 +2,9 @@
 include("../model/conexion2.php");
 $con=conectar();
 
-$id=$_GET['id'];
-
-$sql="DELETE FROM almacen  WHERE id='$id'";
-$query=mysqli_query($con,$sql);
+$id = $_GET['id'];
+$sql = "DELETE FROM almacen WHERE id = '$id'";
+$query = mysqli_query($con,$sql);
 
 if ($query) {
         Header("Location:../view/home.php");
