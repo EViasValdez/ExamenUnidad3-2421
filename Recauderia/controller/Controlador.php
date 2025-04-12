741ed62bea4e0fa06ba6b23761a2f6fd270da203
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2025-04-11 18:04:10 -0600

    Actualizar Controlador.php

--- Recauderia/controller/Controlador.php
+++ Recauderia/controller/Controlador.php
@@ -19,7 +19,7 @@ $cantidad = $_POST['cantidad'];
 $sql="INSERT INTO `almacen`(`id`, `nombre`, `tipo`, `cantidad`) VALUES('$id','$nombre','$tipo','$cantidad')";
 $query= mysqli_query($con,$sql);
 
-if ($query){
+if ($query) {
     Header("Location:../view/home.php");
 
 } else {
