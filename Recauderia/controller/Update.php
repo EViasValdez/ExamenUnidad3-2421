c2acd07f430842727cf632cdcc53e71ca1f20ce9
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2025-04-14 15:58:29 -0600

    Actualizar Update.php

--- Recauderia/controller/Update.php
+++ Recauderia/controller/Update.php
@@ -7,10 +7,10 @@ $nombres = $_POST['nombre'];
 $tipo = $_POST['tipo'];
 $cantidad = $_POST['cantidad'];
 
-$sql="UPDATE almacen SET  nombre='$nombres', tipo='$tipo',cantidad='$cantidad' WHERE id='$id'";
-$query=mysqli_query($con,$sql);
+$sql = "UPDATE almacen SET nombre =  '$nombres', tipo = '$tipo',cantidad = '$cantidad' WHERE id = '$id'";
+$query = mysqli_query($con,$sql);
 
-if($query) {
+if ($query) {
         Header("Location:../view/home.php");
     }
 ?>
